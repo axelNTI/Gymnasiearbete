889157191b6c08f3383f2d605101396aa81857c4
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-09-06 11:25:35 +0200

    2023-09-06

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import time
+import matplotlib
+
+def quicksort(unsorted_list):
+    if len(unsorted_list) <= 1:
+        return unsorted_list
+    randomItem = random.choice(unsorted_list)
+    return (
+        quicksort([i for i in unsorted_list if i < randomItem])
+        + [i for i in unsorted_list if i == randomItem]
+        + quicksort([i for i in unsorted_list if i < randomItem])
+    )
+
+
+time_list = []
+
+for i in range(10):
+    random_list = [random.randint(0, 100000) for i in range(1000000)]
+    start_time = time.time()
+    quicksort(random_list)
+    print(end_time := time.time() - start_time)
+    time_list.append(end_time)
+
+print(sum(time_list) / len(time_list))
