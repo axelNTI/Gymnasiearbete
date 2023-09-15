36d9198a101623621dc7d64423ba3879f55aa7a4
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-09-15 12:52:38 +0200

    2023-09-15

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import random as rand
-import time as t
-import itertools as it
-import matplotlib as mpl
-import numpy as num  # https://math.stackexchange.com/questions/1568900/generating-random-numbers-of-bell-curve-distribution
-import multiprocessing as mp
-# https://pythonspeed.com/articles/faster-multiprocessing-pickle/#:~:text=The%20multiprocessing%20module%20is%20built,benefits%20of%20using%20worker%20processes.
+import matplotlib
+import itertools
+import random
+import numpy
+import time
+import os
+
+random.seed(os.urandom(256))
+
+
 
 
 def quicksort(unsorted_list):
     if len(unsorted_list) <= 1:
         return unsorted_list
-    randomItem = rand.choice(unsorted_list)
+    randomItem = random.choice(unsorted_list)
     return (
         quicksort([i for i in unsorted_list if i < randomItem])
         + [i for i in unsorted_list if i == randomItem]
@@ -23,17 +26,10 @@ def sorting():
     num_of_cycles = 10
     for i in range(num_of_cycles):
         print(f"{i/num_of_cycles * 100}%")
-        random_list = [rand.randint(0, 100000) for i in range(1000000)]
-        start_time = t.time()
+        random_list = [random.randint(0, 100000) for i in range(1000000)]
+        start_time = time.time()
         quicksort(random_list)
-        time_list.append(t.time() - start_time)
-    return time_list
+        time_list.append(time.time() - start_time)
+    print(sum(time_list) / len(time_list))
 
-    
-
-
-if __name__ == "__main__":
-    time_list = []
-    for num in range(mp.cpu_count()):
-        time_list.append(mp.Process(target=sorting).start())
-    print(f"Average: {sum(time_list) / len(time_list)}")
+sorting()
