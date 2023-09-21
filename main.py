0e3e8663982a22c0ec645052b5b6615bce409c2e
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-09-21 22:38:47 +0200

    2023-09-21

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as mplp
+from numba import jit, cuda
 import itertools
 import random
+
+# import timeit
+
 import numpy
 import time
 import os
@@ -19,20 +23,52 @@ def quicksort(unsorted_list):
     )
 
 
-def sorting():
-    time_list = []
-    num_of_cycles = 10
-    size = 1000000
-    for i in range(num_of_cycles):
-        print(f"{i/num_of_cycles * 100}%")
-        random_list = [int(round(random.gauss(0, 3), 0)) for i in range(10)]
-        start_time = time.time()
-        quicksort(random_list)
-        time_list.append(time.time() - start_time)
-    print(sum(time_list) / len(time_list))
-
 random_list = [int(round(random.gauss(0, 3), 0)) for i in range(10)]
-print(random_list)
+
+
+def func():
+    cpu_list = []
+    for i in range(10):
+        start = time.time()
+        quicksort(random_list)
+        cpu_list.append(time.time() - start)
+    print(f"CPU: {sum(cpu_list) / len(cpu_list)}")
+
+
+# function optimized to run on gpu
+@jit(target_backend="cuda")
+def func2():
+    gpu_list = []
+    for i in range(10):
+        start = time.time()
+        quicksort(random_list)
+        gpu_list.append(time.time() - start)
+    print(f"GPU: {sum(gpu_list) / len(gpu_list)}")
+
+
+# print(timeit.timeit('quicksort(random_list)', number=100))
+# print(timeit.timeit('quicksort(random_list)', number=100))
+
+
+if __name__ == "__main__":
+    func()
+    func2()
+
+
+# def sorting():
+#     time_list = []
+#     num_of_cycles = 10
+#     size = 1000000
+#     for i in range(num_of_cycles):
+#         print(f"{i/num_of_cycles * 100}%")
+#         random_list = [int(round(random.gauss(0, 3), 0)) for i in range(10)]
+#         start_time = timeit.timeit()
+#         quicksort(random_list)
+#         time_list.append(time.time() - start_time)
+#     print(sum(time_list) / len(time_list))
+
+# random_list = [int(round(random.gauss(0, 3), 0)) for i in range(10)]
+# print(random_list)
 
 
 # size = 1000000
