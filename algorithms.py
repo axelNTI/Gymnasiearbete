81a522a6f8329e059ff9106fea91b507bb24d5fb
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-09-28 22:10:45 +0200

    2023-09-28

--- algorithms.py
+++ algorithms.py
@@ -18,60 +18,29 @@ def quicksort(unsorted_list) -> list:
     return quicksort(less) + equal + quicksort(greater)
 
 
-# def mergesort(unsorted_list):
-#     if len(unsorted_list) <= 1:
-#         return unsorted_list
-#     left = []
-#     right = []
-#     for i in range(len(unsorted_list)):
-#         if i < (len(unsorted_list)) / 2:
-#             left.append(unsorted_list[i])
-#         else:
-#             right.append(unsorted_list[i])
-#     result = []
-#     while len(right) and len(right):
-#         if left[0] <= right[0]:
-#             result.append(left[0])
-#             left.pop(0)
-#         else:
-#             result.append(right[0])
-#             right.pop(0)
-#     while len(left):
-#         result.append(left[0])
-#         left.pop(0)
-#     while len(right):
-#         result.append(right[0])
-#         right.pop(0)
-#     return result
-
-
-def mergesort(unsorted_list):
+def mergesort(unsorted_list) -> list:
     if len(unsorted_list) <= 1:
         return unsorted_list
     left = mergesort(unsorted_list[: len(unsorted_list) // 2])
     right = mergesort(unsorted_list[len(unsorted_list) // 2 :])
-    result = [i for i in unsorted_list]
+    result = []
     while len(left) and len(right):
-        if left[0] <= right[0]:
-            result.append(left[0])
-            left.pop(0)
-        else:
-            result.append(right[0])
-            right.pop(0)
-
+        result.append((left if left[0] <= right[0] else right).pop(0))
     [result.append(i) for i in left + right]
     return result
 
 
 quick = []
 merge = []
-for i in range(100):
+for i in range(10):
     print(i)
-    to_be_sorted = [int(round(random.gauss(0, 100), 0)) for i in range(10000)]
+    to_be_sorted = [int(round(random.gauss(0, 100), 0)) for i in range(1000000)]
     start = time.time()
+    print('Quick')
     quicksort(to_be_sorted)
     quick.append(time.time() - start)
     start = time.time()
+    print('Merge')
     mergesort(to_be_sorted)
     merge.append(time.time() - start)
 
