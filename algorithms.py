a9b001dc387878099982ed77e0b7effdfa6d3ccd
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-10-18 11:34:00 +0200

    2023-10-18

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,9 @@
 import random
 import time
+from numpy import sign
 
 
-def quicksort(unsorted_list) -> list:
+def quicksort1(unsorted_list) -> list:
     if len(unsorted_list) < 2:
         return unsorted_list
     randomItem = unsorted_list[0]
@@ -15,7 +16,57 @@ def quicksort(unsorted_list) -> list:
         else greater.append(i)
         for i in unsorted_list
     ]
-    return quicksort(less) + equal + quicksort(greater)
+    return quicksort1(less) + equal + quicksort1(greater)
+
+
+def quicksort2(unsorted_list) -> list:
+    if len(unsorted_list) < 2:
+        return unsorted_list
+    randomItem = unsorted_list[0]
+    less, equal, greater = [], [], []
+    [
+        less.append(i)
+        if i < randomItem
+        else equal.append(i)
+        if i == randomItem
+        else greater.append(i)
+        for i in unsorted_list
+    ]
+    return quicksort2(less) + equal + quicksort2(greater)
+
+
+def quicksort3(unsorted_list) -> list:
+    if len(unsorted_list) < 2:
+        return unsorted_list
+    randomItem = unsorted_list[0]
+    less, equal, greater = [], [], []
+    [
+        less.append(i)
+        if i < randomItem
+        else equal.append(i)
+        if i == randomItem
+        else greater.append(i)
+        for i in unsorted_list
+    ]
+    return quicksort3(less) + equal + quicksort3(greater)
+
+
+def quicksort4(unsorted_list) -> list:
+    if len(unsorted_list) < 2:
+        return unsorted_list
+    randomItem = unsorted_list[0]
+    less, equal, greater = [], [], []
+    for i in unsorted_list:
+        locals()[["equal", "greater", "less"][sign(i - randomItem)]].append(i)
+    # [
+    #     less.append(i)
+    #     if i < randomItem
+    #     else equal.append(i)
+    #     if i == randomItem
+    #     else greater.append(i)
+    #     for i in unsorted_list
+    # ]
+    return quicksort4(less) + equal + quicksort4(greater)
 
 
 def mergesort(unsorted_list) -> list:
@@ -42,19 +93,29 @@ def mergesort(unsorted_list) -> list:
     return sorted_list
 
 
-quicksort_list = []
-mergesort_list = []
+quicksort1_list = []
+quicksort2_list = []
+quicksort3_list = []
+quicksort4_list = []
 
 
-for i in range(100):
+for i in range(1000):
     to_be_sorted = [int(round(random.gauss(0, 100), 0)) for i in range(10000)]
-    start = time.perf_counter_ns()
-    quicksort(to_be_sorted)
-    quicksort_list.append(time.perf_counter_ns() - start)
-    start = time.perf_counter_ns()
-    mergesort(to_be_sorted)
-    mergesort_list.append(time.perf_counter_ns() - start)
+    start1 = time.perf_counter_ns()
+    quicksort1(to_be_sorted)
+    quicksort1_list.append(time.perf_counter_ns() - start1)
+    start2 = time.perf_counter_ns()
+    quicksort2(to_be_sorted)
+    quicksort2_list.append(time.perf_counter_ns() - start2)
+    start3 = time.perf_counter_ns()
+    quicksort3(to_be_sorted)
+    quicksort3_list.append(time.perf_counter_ns() - start3)
+    start4 = time.perf_counter_ns()
+    quicksort4(to_be_sorted)
+    quicksort4_list.append(time.perf_counter_ns() - start4)
 
 
-print(f"Quicksort: {sum(quicksort_list) / (1000000*len(quicksort_list))}")
-print(f"Mergesort: {sum(mergesort_list) / (1000000*len(mergesort_list))}")
+print(f"Quicksort1: {sum(quicksort1_list) / (1000000*len(quicksort1_list))}")
+print(f"Quicksort2: {sum(quicksort2_list) / (1000000*len(quicksort2_list))}")
+print(f"Quicksort3: {sum(quicksort3_list) / (1000000*len(quicksort3_list))}")
+print(f"Quicksort4: {sum(quicksort4_list) / (1000000*len(quicksort4_list))}")
