aafa7992d8877ae8901d4b8f80210f298b8bec4c
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-10-18 22:28:54 +0200

    2023-10-18

--- algorithms.py
+++ algorithms.py
@@ -5,17 +5,36 @@ import time
 def quicksort(unsorted_list) -> list:
     if len(unsorted_list) < 2:
         return unsorted_list
+
+    mid = len(unsorted_list) // 2
+    if unsorted_list[0] > unsorted_list[mid]:
+        unsorted_list[0], unsorted_list[mid] = unsorted_list[mid], unsorted_list[0]
+    if unsorted_list[0] > unsorted_list[-1]:
+        unsorted_list[0], unsorted_list[-1] = unsorted_list[-1], unsorted_list[0]
+    if unsorted_list[mid] > unsorted_list[-1]:
+        unsorted_list[mid], unsorted_list[-1] = unsorted_list[-1], unsorted_list[mid]
+    pivot = unsorted_list[mid]
+    left, equal, right = [], [], []
+    for i in unsorted_list:
+        if i < pivot:
+            left.append(i)
+        elif i == pivot:
+            equal.append(i)
+        elif i > pivot:
+            right.append(i)
+    return quicksort(left) + equal + quicksort(right)
+
+
+def quicksort2(unsorted_list) -> list:
+    if len(unsorted_list) < 2:
+        return unsorted_list
     randomItem = unsorted_list[0]
     less, equal, greater = [], [], []
     [
-        less.append(i)
-        if i < randomItem
-        else equal.append(i)
-        if i == randomItem
-        else greater.append(i)
+        [equal, greater, less][numpy.sign(i - randomItem)].append(i)
         for i in unsorted_list
     ]
-    return quicksort(less) + equal + quicksort(greater)
+    return quicksort2(less) + equal + quicksort2(greater)
 
 
 def mergesort(unsorted_list) -> list:
@@ -67,16 +86,7 @@ def cubesort(unsorted_list) -> list:
 
 
 def treesort(unsorted_list) -> list:
-    # Build BST
-    if not len(unsorted_list):
-        return unsorted_list
-    root = node(unsorted_list[0])
-    for i in range(1,len(unsorted_list)):
-        root.insert(unsorted_list[i])
-    # Traverse BST in order. 
-    res = []
-    inorder(root,res)
-    return res
+    pass
 
 
 def librarysort(unsorted_list) -> list:
@@ -96,6 +106,7 @@ def tournamentsort(unsorted_list) -> list:
 
 
 quicksort_list = []
+quicksort2_list = []
 mergesort_list = []
 introsort_list = []
 heapsort_list = []
@@ -116,62 +127,65 @@ for i in range(1000):
     quicksort(to_be_sorted)
     quicksort_list.append(time.perf_counter_ns() - quick_start)
 
-    merge_start = time.perf_counter_ns()
-    mergesort(to_be_sorted)
-    mergesort_list.append(time.perf_counter_ns() - merge_start)
+    # quick_start2 = time.perf_counter_ns()
+    # quicksort2(to_be_sorted)
+    # quicksort2_list.append(time.perf_counter_ns() - quick_start2)
 
-    intro_start = time.perf_counter_ns()
-    introsort(to_be_sorted)
-    introsort_list.append(time.perf_counter_ns() - intro_start)
+    # merge_start = time.perf_counter_ns()
+    # mergesort(to_be_sorted)
+    # mergesort_list.append(time.perf_counter_ns() - merge_start)
 
-    heap_start = time.perf_counter_ns()
-    heapsort(to_be_sorted)
-    heapsort_list.append(time.perf_counter_ns() - heap_start)
+    # intro_start = time.perf_counter_ns()
+    # introsort(to_be_sorted)
+    # introsort_list.append(time.perf_counter_ns() - intro_start)
 
-    block_start = time.perf_counter_ns()
-    blocksort(to_be_sorted)
-    blocksort_list.append(time.perf_counter_ns() - block_start)
+    # heap_start = time.perf_counter_ns()
+    # heapsort(to_be_sorted)
+    # heapsort_list.append(time.perf_counter_ns() - heap_start)
 
-    tim_start = time.perf_counter_ns()
-    timsort(to_be_sorted)
-    timsort_list.append(time.perf_counter_ns() - tim_start)
+    # block_start = time.perf_counter_ns()
+    # blocksort(to_be_sorted)
+    # blocksort_list.append(time.perf_counter_ns() - block_start)
 
-    cube_start = time.perf_counter_ns()
-    cubesort(to_be_sorted)
-    cubesort_list.append(time.perf_counter_ns() - cube_start)
+    # tim_start = time.perf_counter_ns()
+    # timsort(to_be_sorted)
+    # timsort_list.append(time.perf_counter_ns() - tim_start)
 
-    tree_start = time.perf_counter_ns()
-    treesort(to_be_sorted)
-    treesort_list.append(time.perf_counter_ns() - tree_start)
+    # cube_start = time.perf_counter_ns()
+    # cubesort(to_be_sorted)
+    # cubesort_list.append(time.perf_counter_ns() - cube_start)
 
-    library_start = time.perf_counter_ns()
-    librarysort(to_be_sorted)
-    librarysort_list.append(time.perf_counter_ns() - library_start)
+    # tree_start = time.perf_counter_ns()
+    # treesort(to_be_sorted)
+    # treesort_list.append(time.perf_counter_ns() - tree_start)
 
-    patience_start = time.perf_counter_ns()
-    patiencesort(to_be_sorted)
-    patiencesort_list.append(time.perf_counter_ns() - patience_start)
+    # library_start = time.perf_counter_ns()
+    # librarysort(to_be_sorted)
+    # librarysort_list.append(time.perf_counter_ns() - library_start)
 
-    smooth_start = time.perf_counter_ns()
-    smoothsort(to_be_sorted)
-    smoothsort_list.append(time.perf_counter_ns() - smooth_start)
+    # patience_start = time.perf_counter_ns()
+    # patiencesort(to_be_sorted)
+    # patiencesort_list.append(time.perf_counter_ns() - patience_start)
 
-    tournament_start = time.perf_counter_ns()
-    tournamentsort(to_be_sorted)
-    tournesort_list.append(time.perf_counter_ns() - tournament_start)
+    # smooth_start = time.perf_counter_ns()
+    # smoothsort(to_be_sorted)
+    # smoothsort_list.append(time.perf_counter_ns() - smooth_start)
+
+    # tournament_start = time.perf_counter_ns()
+    # tournamentsort(to_be_sorted)
+    # tournesort_list.append(time.perf_counter_ns() - tournament_start)
 
 
 print(f"Quicksort: {sum(quicksort_list) / (1000000*len(quicksort_list))}")
-print(f"Mergesort: {sum(mergesort_list) / (1000000*len(mergesort_list))}")
-print(f"Introsort: {sum(introsort_list) / (1000000*len(introsort_list))}")
-print(f"Heapsort: {sum(heapsort_list) / (1000000*len(heapsort_list))}")
-print(f"Blocksort: {sum(blocksort_list) / (1000000*len(blocksort_list))}")
-print(f"Timsort: {sum(timsort_list) / (1000000*len(timsort_list))}")
-print(f"Cubesort: {sum(cubesort_list) / (1000000*len(cubesort_list))}")
-print(f"Treesort: {sum(treesort_list) / (1000000*len(treesort_list))}")
-print(f"Librarysort: {sum(librarysort_list) / (1000000*len(librarysort_list))}")
-print(f"Patiencesort: {sum(patiencesort_list) / (1000000*len(patiencesort_list))}")
-print(f"Smoothsort: {sum(smoothsort_list) / (1000000*len(smoothsort_list))}")
-print(
-    f"Tournamentsort: {sum(tournesort_list) / (1000000*len(tournesort_list))}"
-)
+# print(f"Quicksort2: {sum(quicksort2_list) / (1000000*len(quicksort2_list))}")
+# print(f"Mergesort: {sum(mergesort_list) / (1000000*len(mergesort_list))}")
+# print(f"Introsort: {sum(introsort_list) / (1000000*len(introsort_list))}")
+# print(f"Heapsort: {sum(heapsort_list) / (1000000*len(heapsort_list))}")
+# print(f"Blocksort: {sum(blocksort_list) / (1000000*len(blocksort_list))}")
+# print(f"Timsort: {sum(timsort_list) / (1000000*len(timsort_list))}")
+# print(f"Cubesort: {sum(cubesort_list) / (1000000*len(cubesort_list))}")
+# print(f"Treesort: {sum(treesort_list) / (1000000*len(treesort_list))}")
+# print(f"Librarysort: {sum(librarysort_list) / (1000000*len(librarysort_list))}")
+# print(f"Patiencesort: {sum(patiencesort_list) / (1000000*len(patiencesort_list))}")
+# print(f"Smoothsort: {sum(smoothsort_list) / (1000000*len(smoothsort_list))}")
+# print(f"Tournamentsort: {sum(tournesort_list) / (1000000*len(tournesort_list))}")
