40384718b6836d4fc2e4169e879779b0cd782203
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-11-22 21:27:38 +0100

    2023-11-22

--- mpl.py
+++ mpl.py
@@ -11,3 +11,28 @@ class Result:
 
 with open("data.pkl", "rb") as file:
     results_list = pickle.load(file)
+
+algorithms = ("Quicksort", "Mergesort", "Heapsort", "Timsort")
+sizes = (1000, 10000, 100000)
+deviations = (0.1, 0.01, 0.001)
+# list_of_lists = []
+#
+# for i in algorithms:
+#     for j in sizes:
+#         for k in deviations:
+#             locals()[f"{i}_{j}_{k}"] = [
+#                 l
+#                 for l in results_list
+#                 if l.algorithm == i and l.size == j and l.deviation == k
+#             ]
+#             list_of_lists.append(locals()[f"{i}_{j}_{k}"])
+list_of_lists = {
+    f"{i}_{j}_{k}": [
+        l for l in results_list if l.algorithm == i and l.size == j and l.deviation == k
+    ]
+    for i in algorithms
+    for j in sizes
+    for k in deviations
+}
+for i, j in [(i, j) for i in sizes for j in deviations]:
+    print(f"{i}_{j}")
