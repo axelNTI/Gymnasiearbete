f551ca4eead3d7e32083ed84a4706b40807010a5
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-11-29 11:32:08 +0100

    2023-11-29

--- mpl.py
+++ mpl.py
@@ -1,5 +1,7 @@
 import pickle
 
+# import matplotlib
+
 
 class Result:
     def __init__(self, time_ns, algorithm, size, deviation) -> None:
@@ -12,27 +14,29 @@ class Result:
 with open("data.pkl", "rb") as file:
     results_list = pickle.load(file)
 
+
+def average(arr: list) -> float:
+    return sum(arr) / len(arr)
+
+
 algorithms = ("Quicksort", "Mergesort", "Heapsort", "Timsort")
 sizes = (1000, 10000, 100000)
 deviations = (0.1, 0.01, 0.001)
-# list_of_lists = []
-#
-# for i in algorithms:
-#     for j in sizes:
-#         for k in deviations:
-#             locals()[f"{i}_{j}_{k}"] = [
-#                 l
-#                 for l in results_list
-#                 if l.algorithm == i and l.size == j and l.deviation == k
-#             ]
-#             list_of_lists.append(locals()[f"{i}_{j}_{k}"])
 list_of_lists = {
-    f"{i}_{j}_{k}": [
-        l for l in results_list if l.algorithm == i and l.size == j and l.deviation == k
-    ]
-    for i in algorithms
-    for j in sizes
-    for k in deviations
+    f"{i}_{j}": {
+        k: average(
+            [
+                l.time_ns * 1e-6
+                for l in results_list
+                if l.size == i and l.deviation == j and l.algorithm == k
+            ]
+        )
+        for k in algorithms
+    }
+    for i in sizes
+    for j in deviations
 }
-for i, j in [(i, j) for i in sizes for j in deviations]:
-    print(f"{i}_{j}")
+categories = [f"{i}_{j}" for i in sizes for j in deviations]
+for i in categories:
+    list_of_lists[i] = dict(sorted(list_of_lists[i].items(), key=lambda x: x[1]))
+    print(list_of_lists[i])
